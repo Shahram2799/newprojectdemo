8d2110a820fd10969ba1deeb8f00c41cef0aa246
shahram2799 <shahram.rahman7@gmail.com>
2025-09-23 17:21:29 -0400

    Update

--- script.js
+++ script.js
@@ -8,3 +8,34 @@ function navigateTo(pageId) {
     document.getElementById(pageId).classList.add('active');
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+  const popoverLinks = document.querySelectorAll('.popoverlink');
+  popoverLinks.forEach(link => {
+    new bootstrap.Popover(link, {
+      trigger: 'hover',
+      placement: 'right',
+      container: 'body'
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => {
+    const videoIframe = modal.querySelector("iframe");
+    const originalSrc = videoIframe ? videoIframe.src : null;
+     if (videoIframe && originalSrc) {
+        modal.addEventListener("hide.bs.modal", () => {
+        videoIframe.src = ""; // Clear video source on close
+        });
+        modal.addEventListener("shown.bs.modal", () => {
+        videoIframe.src = originalSrc; // Restore video source on open
+        });
+    }
+    });
+});
